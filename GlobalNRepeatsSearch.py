41e28b96368eba79ebde3852ddc587407be74cd4
michael51097 <64818085+michael51097@users.noreply.github.com>
2021-01-15 17:46:53 -0800

    Update GlobalNRepeatsSearch.py

--- GlobalNRepeatsSearch.py
+++ GlobalNRepeatsSearch.py
@@ -5,7 +5,18 @@ Created on Sun Oct 18 15:20:05 2020
 @author: Michael
 """
 
-
+"""
+Assignment:
+A repeat is a substring of a DNA sequence that occurs in multiple copies (more 
+than one) somewhere in the sequence. Although repeats can occur on both the 
+forward and reverse strands of the DNA sequence, we will only consider repeats 
+on the forward strand here. Also we will allow repeats to overlap themselves. 
+For example, the sequence ACACA contains two copies of the sequence ACA - once 
+at position 1 (index 0 in Python), and once at position 3. Given a length n, 
+your program should be able to identify all repeats of length n in all sequences
+ in the FASTA file. Your program should also determine how many times each 
+ repeat occurs in the file, and which is the most frequent repeat of a given length.
+"""
 
 #Loads fasta file into a dictionary where keys are ids and values are sequences
 def load_fasta(filename):
