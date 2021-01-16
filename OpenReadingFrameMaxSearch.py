1f26b21fb5f1091d0efaac01e281c2486c58e952
michael51097 <64818085+michael51097@users.noreply.github.com>
2021-01-15 17:47:15 -0800

    Update OpenReadingFrameMaxSearch.py

--- OpenReadingFrameMaxSearch.py
+++ OpenReadingFrameMaxSearch.py
@@ -4,8 +4,16 @@ Created on Sun Oct 18 16:32:13 2020
 
 @author: Michael
 """
-
-
+"""
+Assignment: 
+Given an input reading frame on the forward strand (1, 2, or 3) your program
+ should be able to identify all ORFs present in each sequence of the FASTA 
+ file, and answer the following questions: what is the length of the longest 
+ ORF in the file? What is the identifier of the sequence containing the longest 
+ ORF? For a given sequence identifier, what is the longest ORF contained in 
+ the sequence represented by that identifier? What is the starting position 
+ of the longest ORF in the sequence that contains it?
+"""
 
 
 
